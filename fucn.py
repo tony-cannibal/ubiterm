adb2ca89ef6ca8b4895c0a3f6fb811b3ee52bde1
tony-cannibal <castrovital.luis@gmail.com>
2023-11-21 17:16:56 -0600

    Update

--- fucn.py
+++ fucn.py
@@ -16,3 +16,90 @@ def getTerminals(material: list, celdas: dict) -> tuple[list, list]:
         else:
             m2.append(i)
     return m1, m2
+
+
+def getTotalTerm(terms: list, tipo: dict, moq: dict) -> list:
+    days = []
+    all = []
+    for i in terms:
+        if i[6] not in days:
+            days.append(i[6])
+
+        if i[1] not in all:
+            all.append(i[1])
+    unique = []
+    for i in sorted(all):
+        unique.append([i])
+
+    for i in unique:
+        total = 0
+        average = 0
+        packing = 0
+        stock = 0
+        for x in terms:
+            if x[1] == i[0]:
+                total += x[3]
+        i.append(tipo[i[0][:-2]])
+        i.append(total)
+        i.append(len(days))
+        average = total / len(days)
+        i.append(average)
+        stock = average * 2
+        i.append(stock)
+        if i[0] in moq:
+            packing = moq[i[0]]
+        else:
+            packing = 0
+        i.append(packing)
+        i.append(round(stock / packing, 3))
+
+    for i in unique:
+        if i[1] == "BE.END" or i[7] < 1:
+            i.append("Poco Uso/Trasera")
+        else:
+            i.append("Rack Grande")
+
+    # for i in unique:
+    #     print(i)
+
+    return unique
+
+
+def getCellUnique(terminals: list) -> list:
+    cells = []
+    unique = []
+    for i in terminals:
+        if i[-1] != "prensas":
+            terminalCelda = f"{i[1]} {i[-1]}"
+            if terminalCelda not in cells:
+                cells.append(terminalCelda)
+        if i[1] not in unique and i[-1] != "prensas":
+            unique.append(i[1])
+
+    terms = []
+    for i in sorted(unique):
+        terms.append([i])
+
+    cellTerms = []
+    for i in cells:
+        cellTerms.append(i.split(" "))
+
+    for i in terms:
+        count = 0
+        for b in cellTerms:
+            if i[0] == b[0]:
+                count += 1
+        i.append(count)
+    uniqueCellTerminal = []
+    for i in terms:
+        if i[1] == 1:
+            for b in cellTerms:
+                if b[0] == i[0]:
+                    uniqueCellTerminal.append(b)
+
+    # for i in terms:
+    #     print(i)
+    for i in uniqueCellTerminal:
+        print(i)
+
+    return uniqueCellTerminal
--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import fucn
 
 material = pd.read_excel("MATERIAL.xlsx").values.tolist()
 celdas = {k: d for [k, v, d] in pd.read_excel("CELDAS.xlsx").values.tolist()}
-moq = pd.read_excel("MOQ.xlsx")
-tipo = pd.read_excel("TIPO.xlsx")
+moq = {k: g for [k, v, d, r, g] in pd.read_excel("MOQ.xlsx").values.tolist()}
+tipo = {k: v for [k, v, d] in pd.read_excel("TIPO.xlsx").values.tolist()}
 
 terminalesM1, terminalesM2 = fucn.getTerminals(material, celdas)
 
-print(terminalesM1)
+matM1: list = fucn.getTotalTerm(terminalesM1, tipo, moq)
+matM2: list = fucn.getTotalTerm(terminalesM2, tipo, moq)
+
+uniqueCellTermsM1 = fucn.getCellUnique(terminalesM1)
+uniqueCellTermsM2 = fucn.getCellUnique(terminalesM2)
+
+# print(terminalesM1)
+
+if __name__ == "__main__":
+    pass
